7d61e567496f1e9658b893bd74914e06463d65fa
Rylexium <aleksei.azizov@mail.ru>
2022-05-09 05:36:48 +0400

    input bank card

--- app/src/main/java/com/example/sber_practika/activity/auth/fragments/BankCardFragment.kt
+++ app/src/main/java/com/example/sber_practika/activity/auth/fragments/BankCardFragment.kt
@@ -1,11 +1,13 @@
 package com.example.sber_practika.activity.auth.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import androidx.fragment.app.Fragment
 import com.example.sber_practika.R
 
 
@@ -19,7 +21,40 @@ class BankCardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_bank_card, container, false)
+        val binding = inflater.inflate(R.layout.fragment_bank_card, container, false)
+        binding.findViewById<EditText>(R.id.edittext_bankcard).addTextChangedListener (object : TextWatcher {
+            private val space = ' '
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable) {
+                // Remove all spacing char
+                var pos = 0
+                while (true) {
+                    if (pos >= s.length) break
+                    if (space == s[pos] && ((pos + 1) % 5 != 0 || pos + 1 == s.length)) {
+                        s.delete(pos, pos + 1)
+                    } else {
+                        pos++
+                    }
+                }
+
+                // Insert char where needed.
+                pos = 4
+                while (true) {
+                    if (pos >= s.length) break
+                    val c = s[pos]
+                    // Only if its a digit where there should be a space we insert a space
+                    if ("0123456789".indexOf(c) >= 0) {
+                        s.insert(pos, "" + space)
+                    }
+                    pos += 5
+                }
+            }
+        })
+        return binding
     }
 
     override fun onPause() {
